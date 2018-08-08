61dc98f4d07b7145ee514d66f133bdeb5394ffbb
Hao Hou <ghost89413@gmail.com>
2018-08-08 16:43:37 -0600

    Add some to do draft

---
+++ src/reader/exec.rs
@@ -0,0 +1,11 @@
+/*
+ * Copyright (C) 2018, Hao Hou
+ */
+/*
+ * TODO
+use std::process::{Command, Stdio};
+use reader::linetext::{LineTextReader, LineParser};
+
+pub struct ExecReader<TParser : LineParser> {
+    line_reader: LineTextReader<TParser>,
+}*/
---
+++ src/reader/regparser.rs
@@ -0,0 +1,5 @@
+/*
+ * Copyright (C) 2018, Hao Hou
+ */
+
+// TODO
