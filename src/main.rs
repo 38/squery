31ef2b34dc05fdedfc0e841a099a8b9e4a884dc9
Hao Hou <ghost89413@gmail.com>
2018-08-08 20:11:02 -0600

    haha

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,23 @@ fn main() {
     let schema = ".lines:Int .words:Int .chars:Int .file:String".to_string();
     //let br = std::io::BufReader::new("plumber 12345 1.0\n".as_bytes());
     
-    if let Some(mut parser) = ExecReader::create("wc", &["src/main.rs", "src/reader/exec.rs"], 0, &schema, SepValParser::create(&" \t\n".to_string()))
+    let files = ["./src/table/mod.rs",
+        "./src/table/input.rs",
+        "./src/table/primitive.rs",
+        "./src/table/output.rs",
+        "./src/table/schema.rs",
+        "./src/table/table.rs",
+        "./src/table/row.rs",
+        "./src/writer/mod.rs",
+        "./src/writer/tablewriter.rs",
+        "./src/reader/linetext.rs",
+        "./src/reader/mod.rs",
+        "./src/reader/regparser.rs",
+        "./src/reader/svparser.rs",
+        "./src/reader/exec.rs",
+        "./src/main.rs"];
+    
+    if let Some(mut parser) = ExecReader::create("wc", &files, 0, &schema, SepValParser::create(&" \t\n".to_string()))
     {
 
         let schema = parser.determine_table_schema();
